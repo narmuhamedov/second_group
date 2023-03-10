356cc706aa9f372fa8f9fb160c9a2d9d552c4fc8
rz <kingradomir@gmail.com>
2023-03-10 17:04:29 +0600

    'create_tv_show'

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('blog/', views.blogview, name='blog'),
     path('blog/<int:id>/', views.blogdetailview, name='blog_detail'),
+    path('add-tv/', views.create_post_view, name='create'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from . import models
+from . import models, forms
 
 def blogview(request):
     post = models.Post.objects.all()
@@ -9,3 +10,16 @@ def blogview(request):
 def blogdetailview(request, id):
     post_detail = get_object_or_404(models.Post, id=id)
     return render(request, 'post_list_detail.html', {'post_detail': post_detail})
+
+#create post
+def create_post_view(request):
+    method = request.method
+    form = forms.ShowForm
+    if method == "POST":
+        form = forms.ShowForm(request.POST, request.FILES)
+        form.save()
+        return HttpResponse('Фильм успешно добавлен!')
+    else:
+        form = forms.ShowForm()
+
+    return render(request, 'add_show.html', {'form':form})
