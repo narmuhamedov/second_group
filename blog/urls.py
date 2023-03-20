fbb8fc520a390751a6f61a00fa812516d9132149
rz <kingradomir@gmail.com>
2023-03-20 11:46:32 +0600

    'CRUD'

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("blog/", views.blogview, name="blog"),
     path("blog/<int:id>/", views.blogdetailview, name="blog_detail"),
+    path("blog/<int:id>/update/", views.tvshoupdateview, name='update'),
+    path("blog/<int:id>/delete/", views.deleteblogview, name='delete'),
     path("add-tvshow/", views.create_tvshow_view, name="create"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -24,3 +24,24 @@ def create_tvshow_view(request):
     else:
         form = forms.TvShowForm()
     return render(request, "add-tvshow.html", {"form": form})
+
+#update
+def tvshoupdateview(request, id):
+    blog_object = get_object_or_404(models.Post, id=id)
+    if request.method == 'POST':
+        form = forms.TvShowForm(instance=blog_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Блог успешно обновлен')
+
+    else:
+        form = forms.TvShowForm(instance=blog_object)
+    return render(request, 'updateblog.html', {"form": form,
+                                               "object": blog_object
+                                               })
+
+#delete
+def deleteblogview(request, id):
+    blog_object = get_object_or_404(models.Post, id=id)
+    blog_object.delete()
+    return HttpResponse('Блог успешно удален')
