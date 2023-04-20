90b0ce3b1a1fbffd449d616d891b9d0c4704f134
rz <kingradomir@gmail.com>
2023-04-20 13:32:33 +0600

    'd'

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("blog/<int:id>/", views.PostDetailView.as_view(), name="blog_detail"),
     path("blog/<int:id>/update/", views.BlogUpdateView.as_view(), name='update'),
     path("blog/<int:id>/delete/", views.BlogDeleteView.as_view(), name='delete'),
-    path("add-tvshow/", views.BlogCreateView.as_view(), name="create"),
+    path("add-tvshow/", views.BlogCreateView.as_view(), name="created"),
     path("search/", views.Search.as_view(), name='search'),
 ]
