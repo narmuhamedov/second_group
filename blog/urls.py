89a637e82eb7ae54fbbef07dab7ed03fc2e88446
rz <kingradomir@gmail.com>
2023-04-20 13:06:11 +0600

    'second'

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("blog/", views.BlogView.as_view(), name="blog"),
+    path("", views.BlogView.as_view(), name="blog"),
     path("blog/<int:id>/", views.PostDetailView.as_view(), name="blog_detail"),
     path("blog/<int:id>/update/", views.BlogUpdateView.as_view(), name='update'),
     path("blog/<int:id>/delete/", views.BlogDeleteView.as_view(), name='delete'),
